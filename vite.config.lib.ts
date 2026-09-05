920e7558b35a33f8f9ccdadbccfb845ea04a6c71
taka <tomo05061@gmail.com>
2026-09-05 22:40:52 +0900

    update: build設定見直し

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,19 +1,37 @@
+import minifyHTML from "@lit-labs/rollup-plugin-minify-html-literals";
+import { transform } from "esbuild";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	root: ".",
 	build: {
-		outDir: "./dist/backend/assets/lib",
+		outDir: "./assets/lib",
 		emptyOutDir: true,
+		minify: true,
+		cssCodeSplit: false,
 		lib: {
 			entry: "src/components/OGPCard/index.ts",
 			name: "OgpCard",
 			fileName: (format) => `ogp-card.${format}.js`,
 			formats: ["es", "umd"],
 		},
-		cssCodeSplit: false,
 	},
-	esbuild: {
-		target: "es2022",
-	},
+	plugins: [
+		minifyHTML(),
+		{
+			name: "minify-whitespace-only",
+			enforce: "post",
+			async generateBundle(_, bundle) {
+				for (const chunk of Object.values(bundle)) {
+					if (chunk.type === "chunk") {
+						const result = await transform(chunk.code, {
+							minifyWhitespace: true,
+							loader: "js",
+						});
+						chunk.code = result.code;
+					}
+				}
+			},
+		},
+	],
 });
